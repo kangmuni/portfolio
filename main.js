4a1a4b2e53843592e97c54a40e3649704367e60c
kangmuni <kangmoonee@gmail.com>
2022-05-12 08:53:33 +0900

    JS:Home transparent

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
+// 스크롤 하면 navbar 메뉴 상단바 나타나게 하기
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
@@ -11,6 +11,7 @@ document.addEventListener('scroll', () => {
   }
 });
 
+// navbar 메뉴 클릭 하면 해당 스크롤 뷰로 이동 하기
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
@@ -19,10 +20,17 @@ navbarMenu.addEventListener('click', (event) => {
   if (link === null) {
     return;
   }
-  const scrollTo = document.querySelector(link);
   scrollIntoView(link);
 });
 
+// 스크롤 했을때 home 컨데이너 투명도 조절 하기
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight; //
+});
+
+// *
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
